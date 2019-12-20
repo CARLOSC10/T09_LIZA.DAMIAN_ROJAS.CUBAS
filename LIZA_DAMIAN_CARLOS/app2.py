d834ce1717a0365e2fa94e45b648c500c429a766
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 12:50:35 -0500

    se agrego el 2do ejercicio app2.py

---
+++ LIZA_DAMIAN_CARLOS/app2.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#ARGUMENTOS
+velo=int(os.sys.argv[1])
+tiem=int(os.sys.argv[2])
+
+distacia=libreria.distancia(velo,tiem)
+print("LA DISTACIA DE UN CARRO ES:",distacia, "m")
