6e04020f87f0137958de94639db0906057e56463
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 12:52:08 -0500

    se agrego el 5to ejercicio app5.py

---
+++ LIZA_DAMIAN_CARLOS/app5.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#ARGUMENTOS
+cad=os.sys.argv[1]
+
+cadena=libreria.convertir(cad)
+print("CADENA ESCRITA POR TECLADO LO CONVIERTE DE LETRAS MINUSCULAS A MAYUSCULAS:",cadena)
