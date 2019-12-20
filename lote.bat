28bdb5c5fcdc065966dd47e99a1f8305687a4903
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 13:07:57 -0500

    se agrego el lote.bat

---
+++ lote.bat
@@ -0,0 +1,181 @@
+python LIZA_DAMIAN_CARLOS/app1.py 2 2 2
+python LIZA_DAMIAN_CARLOS/app1.py -5 8 3
+python LIZA_DAMIAN_CARLOS/app1.py 15 19 4
+
+python LIZA_DAMIAN_CARLOS/app2.py 5 5
+python LIZA_DAMIAN_CARLOS/app2.py -10 500
+python LIZA_DAMIAN_CARLOS/app2.py 100  894
+
+python LIZA_DAMIAN_CARLOS/app3.py 2019 1998
+python LIZA_DAMIAN_CARLOS/app3.py 2019 1978
+python LIZA_DAMIAN_CARLOS/app3.py 2019 2004
+
+python LIZA_DAMIAN_CARLOS/app4.py 29 457
+python LIZA_DAMIAN_CARLOS/app4.py 4 2847
+python LIZA_DAMIAN_CARLOS/app4.py 259 784
+
+python LIZA_DAMIAN_CARLOS/app5.py "hola mundo de la programacion"
+python LIZA_DAMIAN_CARLOS/app5.py "liza damian carlos"
+python LIZA_DAMIAN_CARLOS/app5.py "programacion"
+
+python LIZA_DAMIAN_CARLOS/app6.py 51 54
+python LIZA_DAMIAN_CARLOS/app6.py 13 48
+python LIZA_DAMIAN_CARLOS/app6.py 47 78
+
+python LIZA_DAMIAN_CARLOS/app7.py 788 5
+python LIZA_DAMIAN_CARLOS/app7.py 70 487
+python LIZA_DAMIAN_CARLOS/app7.py 785 17
+
+python LIZA_DAMIAN_CARLOS/app8.py 294 457
+python LIZA_DAMIAN_CARLOS/app8.py 44 847
+python LIZA_DAMIAN_CARLOS/app8.py 249 78478
+
+python LIZA_DAMIAN_CARLOS/app9.py 1 1
+python LIZA_DAMIAN_CARLOS/app9.py 45 88
+python LIZA_DAMIAN_CARLOS/app9.py 458 178
+
+python LIZA_DAMIAN_CARLOS/app10.py "PROGRAMACION"
+python LIZA_DAMIAN_CARLOS/app10.py "HOLA MUNDO"
+python LIZA_DAMIAN_CARLOS/app10.py "CARLOS RONALDO"
+
+python LIZA_DAMIAN_CARLOS/app11.py 788 7 58
+python LIZA_DAMIAN_CARLOS/app11.py 45 47 4
+python LIZA_DAMIAN_CARLOS/app11.py 7 1877 45
+
+python LIZA_DAMIAN_CARLOS/app12.py 27 24 71
+python LIZA_DAMIAN_CARLOS/app12.py 55 81 34
+python LIZA_DAMIAN_CARLOS/app12.py 785 9 4
+
+python LIZA_DAMIAN_CARLOS/app13.py 4774 87
+python LIZA_DAMIAN_CARLOS/app13.py 774 57
+python LIZA_DAMIAN_CARLOS/app13.py 4510 547
+
+python LIZA_DAMIAN_CARLOS/app14.py 78 784
+python LIZA_DAMIAN_CARLOS/app14.py 452 745
+python LIZA_DAMIAN_CARLOS/app14.py 77785 45
+
+python LIZA_DAMIAN_CARLOS/app15.py 58 12
+python LIZA_DAMIAN_CARLOS/app15.py 78 4
+python LIZA_DAMIAN_CARLOS/app15.py 69 5
+
+python LIZA_DAMIAN_CARLOS/app16.py 77 8
+python LIZA_DAMIAN_CARLOS/app16.py 4 7454
+python LIZA_DAMIAN_CARLOS/app16.py 7784 45
+
+python LIZA_DAMIAN_CARLOS/app17.py 78455 741
+python LIZA_DAMIAN_CARLOS/app17.py 775 487
+python LIZA_DAMIAN_CARLOS/app17.py 470 5
+
+python LIZA_DAMIAN_CARLOS/app18.py 4 5 5
+python LIZA_DAMIAN_CARLOS/app18.py 9 5 12
+python LIZA_DAMIAN_CARLOS/app18.py 185 57 78
+
+python LIZA_DAMIAN_CARLOS/app19.py 587 12
+python LIZA_DAMIAN_CARLOS/app19.py 787 4
+python LIZA_DAMIAN_CARLOS/app19.py 6974 5
+
+python LIZA_DAMIAN_CARLOS/app20.py 7 12
+python LIZA_DAMIAN_CARLOS/app20.py 80 78
+python LIZA_DAMIAN_CARLOS/app20.py 77 15
+
+python LIZA_DAMIAN_CARLOS/app21.py 200 18
+python LIZA_DAMIAN_CARLOS/app21.py 1000 10
+python LIZA_DAMIAN_CARLOS/app21.py 5000 2
+
+python LIZA_DAMIAN_CARLOS/app22.py 29 457
+python LIZA_DAMIAN_CARLOS/app22.py 10 -2847
+python LIZA_DAMIAN_CARLOS/app22.py 259 784
+
+python LIZA_DAMIAN_CARLOS/app23.py "MATEMATICA BASICA"
+python LIZA_DAMIAN_CARLOS/app23.py "ANALISIS MATEMATICO"
+python LIZA_DAMIAN_CARLOS/app23.py "INGENERIA ELECTRONICA"
+
+python LIZA_DAMIAN_CARLOS/app24.py 47 58 1
+python LIZA_DAMIAN_CARLOS/app24.py 78 5 78
+python LIZA_DAMIAN_CARLOS/app24.py 45 57 78
+
+python LIZA_DAMIAN_CARLOS/app25.py 20 5
+python LIZA_DAMIAN_CARLOS/app25.py 500 8
+python LIZA_DAMIAN_CARLOS/app25.py 300 2
+
+
+
+
+
+
+
+python ROJASCUBAS\app1.py 10
+python ROJASCUBAS\app1.py 20
+python ROJASCUBAS\app1.py -5
+python ROJASCUBAS\app2.py 15
+python ROJASCUBAS\app2.py 20
+python ROJASCUBAS\app2.py -10
+python ROJASCUBAS\app3.py 10 20 30
+python ROJASCUBAS\app3.py 5 8 5
+python ROJASCUBAS\app3.py -4 -5 0
+python ROJASCUBAS\app4.py 123456
+python ROJASCUBAS\app4.py 95821456
+python ROJASCUBAS\app4.py hola
+python ROJASCUBAS\app5.py na
+python ROJASCUBAS\app5.py 12345
+python ROJASCUBAS\app5.py juan
+python ROJASCUBAS\app6.py 10 20 30
+python ROJASCUBAS\app6.py 5 2 3
+python ROJASCUBAS\app6.py 4 10 20
+python ROJASCUBAS\app7.py holamundo
+python ROJASCUBAS\app7.py 12345
+python ROJASCUBAS\app7.py 971586579
+python ROJASCUBAS\app8.py 10
+python ROJASCUBAS\app8.py 20
+python ROJASCUBAS\app8.py -10
+python ROJASCUBAS\app9.py biembenido
+python ROJASCUBAS\app9.py 50
+python ROJASCUBAS\app9.py hombre
+python ROJASCUBAS\app10.py 50
+python ROJASCUBAS\app10.py 80
+python ROJASCUBAS\app10.py 55.5
+python ROJASCUBAS\app11.py JUAN 95487567 35.2
+python ROJASCUBAS\app11.py ANA 95475122 85.5
+python ROJASCUBAS\app11.py ROSA 954785411 10.2
+python ROJASCUBAS\app12.py 100 10 5
+python ROJASCUBAS\app12.py 50 20 1
+python ROJASCUBAS\app12.py 200 52 4
+python ROJASCUBAS\app13.py 1600
+python ROJASCUBAS\app13.py 6400
+python ROJASCUBAS\app13.py 80000
+python ROJASCUBAS\app14.py lunes
+python ROJASCUBAS\app14.py mates
+python ROJASCUBAS\app14.py domingo
+python ROJASCUBAS\app15.py 10
+python ROJASCUBAS\app15.py -5
+python ROJASCUBAS\app15.py -50
+python ROJASCUBAS\app16.py 12345
+python ROJASCUBAS\app16.py hjoadurti
+python ROJASCUBAS\app16.py 190005G
+python ROJASCUBAS\app17.py RAUL 500
+python ROJASCUBAS\app17.py PAUL 1000
+python ROJASCUBAS\app17.py ROSA 5
+python ROJASCUBAS\app18.py 15 14
+python ROJASCUBAS\app18.py 20 23
+python ROJASCUBAS\app18.py 54 8
+python ROJASCUBAS\app19.py 4 3
+python ROJASCUBAS\app19.py 5 7
+python ROJASCUBAS\app19.py 90 120
+python ROJASCUBAS\app20.py 25 14
+python ROJASCUBAS\app20.py 32 41
+python ROJASCUBAS\app20.py  5 2
+python ROJASCUBAS\app21.py 200 10
+python ROJASCUBAS\app21.py 15 45
+python ROJASCUBAS\app21.py 14 12
+python ROJASCUBAS\app22.py 10 20
+python ROJASCUBAS\app22.py 30 20
+python ROJASCUBAS\app22.py 15 100
+python ROJASCUBAS\app23.py 150
+python ROJASCUBAS\app23.py 15
+python ROJASCUBAS\app23.py 20
+python ROJASCUBAS\app24.py juana 150 15
+python ROJASCUBAS\app24.py andres 50 5
+python ROJASCUBAS\app24.py anita 4 0.5
+python ROJASCUBAS\app25.py fhrfgrgcr
+python ROJASCUBAS\app25.py 1654515
+python ROJASCUBAS\app25.py hjkfghert
