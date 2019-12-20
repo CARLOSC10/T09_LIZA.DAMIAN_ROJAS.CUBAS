ac391879834ce5e4f8fd36d7eaa94699c3e19d1e
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 12:49:57 -0500

    se agrego el 1er ejercicio app1.py

---
+++ LIZA_DAMIAN_CARLOS/app1.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+#ARGUMENTOS
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+c=int(os.sys.argv[3])
+
+area=libreria.area_trapecio(a,b,c)
+print("EL AREA DEL TRAPECIO ES:",area)
