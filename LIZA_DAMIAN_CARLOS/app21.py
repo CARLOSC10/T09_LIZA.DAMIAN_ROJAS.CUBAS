f1a711dd6cbe20b1e380c781c6a0ca62bf723342
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 12:59:53 -0500

    se agrego el ejercicio 21 app21.py

---
+++ LIZA_DAMIAN_CARLOS/app21.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#ARGUMENTOS
+traba=int(os.sys.argv[1])
+tiem=int(os.sys.argv[2])
+
+potencia=libreria.potencia(traba,tiem)
+print("LA FORMULA DE LA POTENCIA ES:",potencia)
