eb507b93fa7650065ec8de46b61ba9a308913957
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-12-20 12:53:16 -0500

    se agrego el 7mo ejercicio app7.py

---
+++ LIZA_DAMIAN_CARLOS/app7.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#ARGUMENTOS
+late=int(os.sys.argv[1])
+ar=int(os.sys.argv[2])
+
+piramide=libreria.volumen_piramide(late,ar)
+print("VOLUMEN DE UNA PIRAMIDE ES:",piramide)
